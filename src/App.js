65795570dd2aaf06fb70be39bb19ee7b3b83df6b
Eric <eric@schreiterer.eu>
2022-10-09 00:31:42 +0200

    - add material UI - refactor main app to match basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Grid from '@mui/material/Unstable_Grid2';
+import {Box, Typography} from "@mui/material";
+import Image from "mui-image";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+              <Grid item xs={12} mb={4} mt={4} display={"flex"} justifyContent={"center"}>
+                  <span className="headline">Save the Date</span>
+              </Grid>
+              <Grid item xsOffset={3} xs={6} mb={2} display={"flex"} justifyContent={"center"}>
+                  <Image
+                      src="/IMG-8168.jpg"
+                      showLoading={true}
+                      duration={3000}
+                      sx={{borderRadius: "50%"}}
+                      width="200px"
+                      height="200px"
+                  />
+              </Grid>
+              <Grid item xs={12} display={"flex"} justifyContent={"center"}>
+                  <span className="main-text">welcome to the wedding celebrations of</span>
+              </Grid>
+              <Grid item xs={12} display={"flex"} justifyContent={"center"}>
+                  <span className="couple-name">ERIC</span>
+              </Grid>
+              <Grid item xs={12} display={"flex"} justifyContent={"center"}>
+                  <span className="main-text">and</span>
+              </Grid>
+              <Grid item xs={12} display={"flex"} justifyContent={"center"}>
+                  <span className="couple-name">IZZAH</span>
+              </Grid>
+          </Grid>
+      </Box>
   );
 }
 
